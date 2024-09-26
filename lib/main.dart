d130e630f34be8f0ec1fb8260cacf6be5134ba02
Neo <speldipn@gmail.com>
2024-09-26 20:41:34 +0900

    webview example

--- lib/main.dart
+++ lib/main.dart
@@ -2,74 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(SplashScreen());
+  runApp(WebViewExample());
 }
 
-class SplashScreen extends StatelessWidget {
+// class SplashScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return (MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Stack(
+//           alignment: Alignment.center,
+//           children: [
+//             Container(color: const Color(0xFFF99231)),
+//             Expanded(
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   SizedBox(
+//                     width: 150.0,
+//                     height: 150.0,
+//                     child: Image.asset("assets/logo.png"),
+//                   ),
+//                   const CircularProgressIndicator(
+//                     valueColor: AlwaysStoppedAnimation(Colors.white),
+//                   ),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     ));
+//   }
+// }
+
+class WebViewExample extends StatelessWidget {
+  final baseUrl = "https://goldenrabbit.co.kr/product/must-have-codefactory-flutter";
+
   @override
   Widget build(BuildContext context) {
+    var controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) { print("load finished ${url}"); },
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(baseUrl));
+
     return (MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(color: const Color(0xFFF99231)),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 150.0,
-                    height: 150.0,
-                    child: Image.asset("assets/logo.png"),
-                  ),
-                  const CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(Colors.white),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Code Factory Flutter'),
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                onPressed: () { controller.loadRequest(Uri.parse(baseUrl)); },
+                icon: const Icon(Icons.home, color: Colors.orange),
+              )
+            ],
+          ),
+          body: WebViewWidget(controller: controller)),
     ));
   }
 }
 
-// class WebViewExample extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     var controller = WebViewController()
-//       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-//       ..setNavigationDelegate(
-//         NavigationDelegate(
-//           onProgress: (int progress) {
-//             // Update loading bar.
-//           },
-//           onPageStarted: (String url) {},
-//           onPageFinished: (String url) {},
-//           onHttpError: (HttpResponseError error) {},
-//           onWebResourceError: (WebResourceError error) {},
-//           onNavigationRequest: (NavigationRequest request) {
-//             if (request.url.startsWith('https://www.youtube.com/')) {
-//               return NavigationDecision.prevent;
-//             }
-//             return NavigationDecision.navigate;
-//           },
-//         ),
-//       )
-//       ..loadRequest(Uri.parse('https://google.co.kr'));
-//
-//     return (MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//           appBar: AppBar(title: const Text('webview_flutter')),
-//           body: WebViewWidget(controller: controller)),
-//     ));
-//   }
-// }
-//
 // class StackWidgetExample extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
